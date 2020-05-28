a182a5dbc959842584f22a62cae6eebff47819b2
Chrisxander <christopheradmon@gmail.com>
2020-05-28 16:49:46 -0500

    primer commit de mi sitio web

---
+++ main.js
@@ -0,0 +1,18 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+function muestra_oculta(id){
+  if (document.getElementById){ //se obtiene el id
+  var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+  el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+  }
+  }
+  window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+  muestra_oculta('contenido');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+  }
